b5108a50d0d81d4391012a6349ae31864149e2dd
mahnoor740 <amirmahnoor327@gmail.com>
2025-12-03 07:56:07 +0500

    frontend and dsa

---
+++ dsa/sorting.py
@@ -0,0 +1,21 @@
+# dsa/sorting.py
+
+def merge_sort(arr, key):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid], key)
+    right = merge_sort(arr[mid:], key)
+    return _merge(left, right, key)
+
+def _merge(left, right, key):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i][key] <= right[j][key]:
+            result.append(left[i]); i += 1
+        else:
+            result.append(right[j]); j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
